024a2283c03a335ff319a889ce8eb26d34174f72
eru123 <yeoligoakino@gmail.com>
2021-12-29 08:01:14 +0800

    added eslint, prettier, vuex and vue-store

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: true,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    printWidth: 80,
+    arrowParens: "always",
+    endOfLine: "auto",
+}
